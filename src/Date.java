1de10ba2801a0b9487369aaaaa44dd03258cba67
rainagupta <rainagupta@192.168.100.114>
2020-02-09 14:21:04 -0500

    asdfghjkl

--- src/Date.java
+++ src/Date.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 /**
   
  @author  Jay Mendapara & Raina Gupta
@@ -10,7 +12,32 @@ public class Date
    
    public Date(String d)
    {
-      //use StringTokenizer to parse the String and create a Date object     
+      //use StringTokenizer to parse the String and create a Date object
+	   
+	   StringTokenizer st = new StringTokenizer(d, "/");
+	   
+	   int counter = 0;
+	   
+	   while (st.hasMoreTokens()) {
+		   
+		   switch (counter) {
+		   
+		   case (0):
+			   this.month = Integer.parseInt(st.nextToken("/"));
+		   
+		   case (1):
+			   this.day = Integer.parseInt(st.nextToken("/"));
+		  
+		   case (2):
+			   this.year = Integer.parseInt(st.nextToken("/"));
+		   
+		   case (3):
+			   //correct error statement? who knows
+			   System.out.println("Invalid date");
+		   
+		   }
+		   
+	   }
    }
    /**
     * 
@@ -20,6 +47,9 @@ public class Date
    public Date(Date d)
    {
       //this is a constructor
+	   this.day = d.day;
+	   this.month = d.month;
+	   this.year = d.year;
    }      
    
    public boolean isValid()
@@ -31,12 +61,31 @@ public class Date
    public String toString()
    {
        //use the format "month/day/year"
+	   
+	   return (this.month + "/" + this.day + "/" + this.year);
    }
    
    @Override
    public boolean equals(Object obj)
    {
-       
+		if(obj instanceof Date){	
+
+			//obj.getClass() == (new TeamMember(name, startDate).getClass())
+			//obj = TeamMember.class.cast(obj);
+
+			Date temp = (Date)obj;
+
+			if ((this.month == temp.month) && (this.day == temp.day) && (this.year == temp.year)) {
+
+				return true;
+
+			}
+
+		}
+		
+		//if obj is not of type Team Date or does not match the current month/date/year
+		return false;
+
    }  
 }
 
