af794b16388cb0d14dc61259db013b46a0c3dba5
Jmendapara <jmendapara@gmail.com>
2020-02-14 14:23:36 -0500

    Oh! Fuck! Shit! Bitch! Young Sheck Wes and I'm getting really rich

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -20,6 +20,8 @@ public class ProjectManager
 	public void run(){
 
 		boolean done = false;
+		
+		cs213 = new Team();
 
 		System.out.println("Welcome!");
 		System.out.println("Your entry must be of the following format: Command (A, R, P, Q) Name Month/Day/Year.");
@@ -36,19 +38,29 @@ public class ProjectManager
 			{   
 			case 'A':
 
-				if (instantiateTeamMember(command)) {
+				String nameAdd = stdin.next();
+				Date dateAdd = new Date(stdin.next());
+				
+				currMem = new TeamMember(nameAdd, dateAdd);
+				
+				add();
+				
+				/*if (instantiateTeamMember(command)) {
 				
 					add();
 					System.out.println("Team Member added succesfully");
 					print();
-				}
+				}*/
 				
 				//break;
 
 			case 'R':
 
-				instantiateTeamMember(command);
-
+				String nameRemove = stdin.next();
+				Date dateRemove = new Date(stdin.next());
+				
+				currMem = new TeamMember(nameRemove, dateRemove);
+				
 				remove();
 				break;
 
@@ -139,10 +151,25 @@ public class ProjectManager
 
 	private void add()
 	{
+		
 		//must check if the date is valid
 		//must call the contains() method to check if a given 
 		//team member is in the team already
 		
+		if(currMem.getStartDate().isValid()) {
+			
+			if(cs213.contains(currMem)) {
+				System.out.println("Team member already exists!");
+			}
+			else{
+				cs213.add(currMem);
+			}
+			
+		}
+		
+		else {
+			System.out.println("Date is invalid");
+		}
 		
 		
 	}
@@ -150,8 +177,17 @@ public class ProjectManager
 	private void remove()
 	{
 		//must check if the date is valid
+		if(currMem.getStartDate().isValid()) {
+
+			cs213.remove(currMem);
 		
-		cs213.remove(currMem);
+		}
+
+		else {
+			
+			System.out.println("Date is invalid");
+			
+		}
 
 	}
 
