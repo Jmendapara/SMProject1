c4373d2cd6764cf5b12d848892cc6ba150ed60e3
rainagupta <rainagupta@192.168.100.114>
2020-02-14 23:17:45 -0500

    testbed and printing is done

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -15,14 +15,13 @@ public class ProjectManager
 	Team cs213;
 
 	private TeamMember currMem;
-	//private Queue<String> tokens = new LinkedList<String>();
 
 	public void run(){
 
 		boolean done = false;
-		
-		cs213 = new Team();
 
+		cs213 = new Team();
+		
 		System.out.println("Welcome!");
 		System.out.println("Your entry must be of the following format: Command (A, R, P, Q) Name Month/Day/Year.");
 
@@ -38,35 +37,38 @@ public class ProjectManager
 			{   
 			case 'A':
 
+				readTeamMember();
+				
+				/*
 				String nameAdd = stdin.next();
 				Date dateAdd = new Date(stdin.next());
-				
+
 				currMem = new TeamMember(nameAdd, dateAdd);
-				
+				*/
+
 				add();
-				
+
 				/*if (instantiateTeamMember(command)) {
-				
+
 					add();
 					System.out.println("Team Member added succesfully");
 					print();
 				}*/
-				
+
 				//break;
 
 			case 'R':
 
 				String nameRemove = stdin.next();
 				Date dateRemove = new Date(stdin.next());
-				
+
 				currMem = new TeamMember(nameRemove, dateRemove);
-				
+
 				remove();
-				break;
+				//break;
 
 			case 'P':
 				print();
-				break;
 
 			case 'Q':
 				done = true;
@@ -86,92 +88,42 @@ public class ProjectManager
 
 	/**
 	 * 
-	 * @param command
 	 */
-	private boolean instantiateTeamMember(String command) {
+	private void readTeamMember() {
 
-		String name;
-		Date date;
-		boolean valid;
-		
-		/*
-		if(!stdin.hasNext()) {
-			System.out.println("Your entry must be of the following format: Command Name Month/Day/Year.");
-			return;
-		}
-		*/
-		
-		//reads in name token
-		String token = stdin.next();
-		System.out.println(token);
-		name = token;
-		
-		//CASE: the user did not enter enough tokens
-		boolean temp = stdin.hasNext("raina");
-		System.out.println(temp);
+		//reading in date and name
+		String name = stdin.next();
+		Date date = new Date(stdin.next());
 
-	/*
-		if(!stdin.hasNext()) {
-			System.out.println("Your entry must be of the following format: Command (A, R, P, Q) Name Month/Day/Year.");
-			return;
-		}
-	*/	
-		
-		//reads in date token
-		token = stdin.next();
-		System.out.println(token);
-		
-
-		//CASE: the user did not enter enough tokens
-
-		/*
-		if (token == null) {
-			System.out.println("Your entry must be of the following format: Command Name Month/Day/Year.");
-			return;
-		}
-		*/
-
-		date = new Date(token);
-
-		valid = date.isValid();
-		
-		if(valid) {	
-			
-			currMem = new TeamMember(name, date);
-
-			System.out.println(currMem.toString());
-			
-			return true;
-		}
-
-		return false;
+		//creating new TeamMember object with console inputs
+		currMem = new TeamMember(name, date);
 
 	}
 
 
 	private void add()
 	{
-		
+
 		//must check if the date is valid
 		//must call the contains() method to check if a given 
 		//team member is in the team already
-		
+
 		if(currMem.getStartDate().isValid()) {
-			
+
 			if(cs213.contains(currMem)) {
 				System.out.println("Team member already exists!");
 			}
 			else{
 				cs213.add(currMem);
 			}
-			
+
 		}
-		
+
 		else {
 			System.out.println("Date is invalid");
 		}
-		
-		
+
+
 	}
 
 	private void remove()
@@ -180,13 +132,13 @@ public class ProjectManager
 		if(currMem.getStartDate().isValid()) {
 
 			cs213.remove(currMem);
-		
+
 		}
 
 		else {
-			
+
 			System.out.println("Date is invalid");
-			
+
 		}
 
 	}
--- src/TeamMember.java
+++ src/TeamMember.java
@@ -13,7 +13,7 @@ public class TeamMember
 		this.startDate = date;
 
 	}
-	
+
 	public Date getStartDate()
 	{
 		return this.startDate;
@@ -30,14 +30,14 @@ public class TeamMember
 
 			TeamMember temp = (TeamMember)obj;
 
-			if (this.name.equals(temp.name) && (this.startDate == temp.startDate)) {
+			if (this.name.equals(temp.name) && (this.startDate.equals(temp.startDate))) {
 
 				return true;
 
 			}
 
 		}
-		
+
 		//if obj is not of type Team Member or does not match the current name/startDate
 		return false;
 
@@ -54,18 +54,95 @@ public class TeamMember
 	{
 		//testbed main; you must include test cases that exercise 
 		//the constructor and all methods in this class.
+
+
+		System.out.println("--------TESTING Date CLASS--------");
+
+		Date testDate1 = new Date("05/32/1999");
+		Date testDate2 = new Date("05/09/1999");
+		Date testDate3 = new Date("02/29/2012");
+		Date testDate4 = new Date("02/29/2013");
+
+
+		boolean valid;
+
+		// TEST - false case for Date isValid()
+		valid = testDate1.isValid();
+		System.out.println("Date : " + testDate1.toString() + " isValid() = " + valid);
+
+		// TEST - true case for Date isValid()
+		valid = testDate2.isValid();
+		System.out.println("Date : " + testDate2.toString() + " isValid() = " + valid);
+
+		// TEST - true case for Date isValid() on a leap year
+		valid = testDate3.isValid();
+		System.out.println("Date : " + testDate3.toString() + " isValid() = " + valid);
 		
-		Date testDate = new Date("05/32/1999");
+		// TEST - false case for Date isValid() on a not leap year
+		valid = testDate4.isValid();
+		System.out.println("Date : " + testDate4.toString() + " isValid() = " + valid);
+
+
+		System.out.println("Date toString : " + testDate2.toString()); //TEST - Date toString
+
 		
-		System.out.println("Date toString : " + testDate.toString()); //TEST - Date toString
+		System.out.println("--------END TESTING Date CLASS--------\n");
+		System.out.println("--------TESTING TeamMember CLASS--------");
+
+		TeamMember test = new TeamMember("Raina", testDate2); //TEST - TeamMember constructor
+
+		Date compareDate1 = new Date("08/13/13");
+		Date compareDate2 = new Date("05/09/1999");
+		boolean result;
+
+		// TEST - equals 
+		TeamMember compare1 = new TeamMember("Raina", compareDate1);
+		result = test.equals(compare1);
+		System.out.println("TEST - this.equals(Object obj) where this = " + test.toString() + " and obj = " + compare1.toString());
+		System.out.println("Result = " + result + "\n");
+
+		TeamMember compare2 = new TeamMember("Raina", compareDate2);
+		result = test.equals(compare2);
+		System.out.println("TEST - this.equals(Object obj) where this = " + test.toString() + " and obj = " + compare2.toString());
+		System.out.println("Result = " + result);
+
+		System.out.println("--------END TESTING TeamMember CLASS--------\n");
+		System.out.println("--------TESTING Team CLASS--------");
+
+		// creating instances of TeamMember
+		TeamMember jay = new TeamMember("Jay", new Date("12/1/2014"));
+		TeamMember bob = new TeamMember("Bob", new Date("9/31/2016")); // Last date (31th) of odd month
+		TeamMember al = new TeamMember("Al", new Date("2/29/2012")); // Leap year date
+		TeamMember jenny = new TeamMember("Jenny", new Date("6/30/2005")); // Last date (30th) of even month
+
+		Team testTeam = new Team();
+
+
+		testTeam.add(jay);
+
+		System.out.println("Adding a team member that already exists:");
+		testTeam.add(bob);
+		testTeam.add(bob); // case - adding a TeamMember twice
+
+		System.out.println("Removing a TeamMember that does not exist:");
+		testTeam.remove(al); // case - removing a TeamMember that is not in Team
+		testTeam.add(al);
+		System.out.println("Removing a TeamMember that does exist:");
+		testTeam.remove(al); // case - removing a TeamMember successfully
+
+		testTeam.contains(al); // case - does not exist
+
+		System.out.println("Checking if team member 'jay' is contained: ");
+
+		System.out.println(testTeam.contains(jay)); // case - returns true;
+
+		testTeam.add(jenny);
+		testTeam.add(test);
+
+		testTeam.add(compare1); // case - have to call grow() on array
+		testTeam.add(compare2); // case - adding with the same name, different date
 		
-		TeamMember test = new TeamMember("Raina", testDate); //TEST - TeamMember constructor
-		
-		
-		
-		
-		
-		
+		System.out.println("-------- END TESTING Team CLASS--------\n");
 	}
 
 }
